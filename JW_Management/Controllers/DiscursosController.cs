3cdbcb3712a1ba5821dcb07d76cb1808fa7465cd
Jorge Monteiro <Jorge Monteiro@DESKTOP-RT73T4G>
2020-02-22 11:59:16 +0000

    Adicionada Modelo, Controlador e View Index dos Discursos!

---
+++ JW_Management/Controllers/DiscursosController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JW_Management.Controllers
+{
+    public class DiscursosController : Controller
+    {
+        // GET: Discursos
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Discursos/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Discursos/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Discursos/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Discursos/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Discursos/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Discursos/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Discursos/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ JW_Management/Models/Discurso.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JW_Management.Models
+{
+    public class Discurso
+    {
+        public int IdDiscurso { get; set; }
+        public string NomeOrador { get; set; }
+        public string CongregacaoOrador { get; set; }
+        public string ContactoOrador { get; set; }
+        public DateTime DataDiscurso { get; set; }
+        public int IdTemaDiscurso { get; set; }
+        public string TemaDiscurso { get; set; }
+
+        public bool Dentro_Fora { get; set; }
+        public string Observacoes { get; set; }
+    }
+}
