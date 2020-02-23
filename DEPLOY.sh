ce6b82a4908b9197bebc2d64a3103ce066738bda
Jorge Monteiro <Jorge Monteiro@DESKTOP-RT73T4G>
2020-02-23 17:28:44 +0000

    Criado script for easy deploy

---
+++ DEPLOY.sh
@@ -0,0 +1,22 @@
+cd ~/JW_Management
+
+git clone https://github.com/JKSProds/JW_Management.git
+
+cd JW_Management/JW_Management
+
+docker build -t jwmanagement .
+
+docker stop JW_Management
+docker rm JW_Management
+
+cd ..
+cd ..
+
+chmod -R 777 JW_Management
+
+rm -r JW_Management
+
+docker run -d -p 8081:80 --name JW_Management jwmanagement
+
+
+
