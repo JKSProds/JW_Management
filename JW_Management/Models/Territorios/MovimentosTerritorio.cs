cb5fd894e6f0df5b1b2dd5497d4793ef676ec118
Jorge Monteiro <Jorge Monteiro@FIXO>
2023-04-14 13:02:18 +0100

    Organizacao e criacao das classes do territorio

---
+++ JW_Management/Models/Territorios/MovimentosTerritorio.cs
@@ -0,0 +1,16 @@
+﻿namespace JW_Management.Models
+{
+    public class MovimentosTerritorio
+    {
+        public string? Stamp { get; set; }
+        public Territorio? Territorio { get; set; }
+        public Publicador? Publicador { get; set; }
+        public DateTime DataMovimento { get; set; }
+        public TipoMovimentoTerritorio Tipo { get; set; }
+    }
+
+    public enum TipoMovimentoTerritorio
+    {
+        ENTRADA, SAIDA
+    }
+}
---
+++ JW_Management/Models/Territorios/Territorio.cs
@@ -0,0 +1,19 @@
+﻿namespace JW_Management.Models
+{
+    public class Territorio
+    {
+        public string? Stamp { get; set; }
+        public string? Id { get; set; }
+        public string? Nome { get; set; }
+        public DificuldadeTerritorio Dificuldade { get; set; }
+        public string? Descricao { get; set; }
+        public string? Url { get; set; }
+        public List<MovimentosTerritorio>? Movimentos { get; set; }
+        public List<string>? Ficheiros { get; set; }
+    }
+
+    public enum DificuldadeTerritorio
+    {
+        FACIL, MODERADO, DIFICIL
+    }
+}
