b5689e25b2aee7b7162720c3c32cb1fa3a985e8c
Jorge Monteiro <Jorge Monteiro@FIXO>
2023-04-27 22:47:17 +0100

    deploy xodo

---
+++ JW_Management/Controllers/XodozinhaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JW_Management.Controllers
+{
+    public class XodozinhaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
